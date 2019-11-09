74229b4a42c8a26158ef11604a62e0774c52d312
im-cuttlefish <firefly.squid.ink@gmail.com>
2019-11-10 01:19:40 +0900

    update

---
+++ webpack.common.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.ts"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.tsx?$/i,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".css", ".jpg", ".jpeg", ".png", ".gif", ".svg"]
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./assets/index.html",
+      favicon: './assets/favicon.png',
+      inject: true
+    })
+  ]
+};
