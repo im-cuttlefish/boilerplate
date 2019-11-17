272b67397fd95ac125fa1ea177777c5490b1d7f3
im-cuttlefish <firefly.squid.ink@gmail.com>
2019-11-18 00:00:18 +0900

    update

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -31,7 +32,10 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js", ".css", ".jpg", ".jpeg", ".png", ".gif", ".svg"]
+    extensions: [".tsx", ".ts", ".js", ".css", ".jpg", ".jpeg", ".png", ".gif", ".svg"],
+    plugins: [
+      new TsconfigPathsPlugin()
+    ]
   },
   output: {
     filename: '[name].bundle.js',
