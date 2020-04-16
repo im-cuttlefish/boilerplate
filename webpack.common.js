193e65b436ba5493c3eb8f4ef040071a604499c8
im-cuttlefish <firefly.squid.ink@gmail.com>
2020-04-16 21:01:28 +0900

    rewrite .eslintrc.json

--- webpack.common.js
+++ webpack.common.js
@@ -1,52 +1,48 @@
 const path = require("path");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
-  entry: {
-    app: "./src/index.ts"
-  },
+  entry: "./src/index",
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.tsx?$/i,
         use: "ts-loader",
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 8192,
             },
           },
         ],
-      }
-    ]
+      },
+    ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js", ".css", ".jpg", ".jpeg", ".png", ".gif", ".svg"],
-    plugins: [
-      new TsconfigPathsPlugin()
-    ]
+    extensions: [".tsx", ".ts", ".js"],
+    plugins: [new TsconfigPathsPlugin()],
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, "dist")
+    filename: ".bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./assets/index.html",
-      favicon: './assets/favicon.png',
-      inject: true
-    })
-  ]
+      template: path.resolve(__dirname, "public/index.html"),
+      favicon: path.resolve(__dirname, "public/favicon.png"),
+      inject: true,
+    }),
+  ],
 };
